bc8e391ce631fbc7676c5f6a48c52d39d658dd52
dongwj5 <dongwj5@SHCYF29T>
2017-04-19 09:51:37 +0800

    1

--- SingleAdd/Program.cs
+++ SingleAdd/Program.cs
@@ -13,7 +13,7 @@ namespace ImageTest
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        { 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SingleAdd());
